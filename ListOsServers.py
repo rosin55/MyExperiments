d2adf0e29d3e5e6c7608fc590c81380097517d46
Rosin <asakhno@gmail.com>
2019-12-10 15:55:33 +0300

    Запрос имени сервера

--- ListOsServers.py
+++ ListOsServers.py
@@ -1,5 +1,12 @@
 ''' Пример выполнения коман операционной системы '''
 
 import os
+import adodbapi
+
 os.system('sqlcmd -L')
-os.system('systeminfo')
+ServerName = input('Имя сервера: ')
+
+conn_string = 'sqlcmd -S' + ServerName + ' -Uhotel3 -P123 -Q "SELECT name, database_id, create_date  FROM sys.databases"'
+print(conn_string)
+os.system(conn_string)
+
