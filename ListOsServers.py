f2739b56b41f4a0d43c826aa2aad36df03606d58
Rosin <asakhno@gmail.com>
2019-12-11 17:20:25 +0300

    Отформатировал вывод и добавил совместимость SQL

--- ListOsServers.py
+++ ListOsServers.py
@@ -5,16 +5,27 @@
 
 import os
 import adodbapi
+from termcolor import colored, cprint  # Цветной терминал
+# Таблица соответствия версии сервера и его названия
+# взято отсюда https://docs.microsoft.com/ru-RU/
+#              sql/relational-databases/system-catalog-views/sys-databases-transact-sql?view=aps-pdw-2016
+v_server = {100: 'SQL Server 2008',
+            110: 'SQL Server 2012',
+            120: 'SQL Server 2014',
+            130: 'SQL Server 2016',
+            140: 'SQL Server 2017'}
 
 os.system('sqlcmd -L')
 ServerName = input('Имя сервера: ')
 
-myConnStr = 'Provider=SQLNCLI11; Data Source=' + ServerName + '; Initial Catalog=h3_prot; User ID=hotel3; Password=123'
+myConnStr = 'Provider=SQLNCLI11; Data Source=' + ServerName + '; Initial Catalog=master; User ID=hotel3; Password=123'
 myConn = adodbapi.connect(myConnStr)
 
 myCursor = myConn.cursor()
-myCursor.execute('SELECT name, database_id, create_date  FROM sys.databases')
+myCursor.execute('SELECT name, database_id, create_date, compatibility_level  FROM sys.databases')
 
+cprint('{:>25} {:<5} {:>10} {:>15}'.format('Имя БД', 'Номер', 'Дата создания', 'Совместимость'), 'yellow', 'on_white')
 for row in myCursor:
-    print(row)
-
+    s_create_date = str(row.create_date)[:10]  # первые 10-ть символов даты
+    sovmestim = v_server[row.compatibility_level]
+    cprint('{:>25} {:<5} {:>10} {:>15}'.format(row.name, row.database_id, s_create_date, sovmestim ))
